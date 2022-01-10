11ba188c4ca0d8d6a5e312839c147a14ad001a0c
Alf-Rune Siqveland <alf.rune@northern.tech>
2022-01-10 11:39:50 +0100

    [QA] Add CLI command test coverage for acceptance tests
    Use the test binary through Docker to gather coverage of acceptance
    tests.
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- main_test.go
+++ main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Northern.tech AS
+// Copyright 2022 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -11,50 +11,98 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
+//go:build main
+// +build main
+
 package main
 
 import (
+	"bufio"
+	"encoding/hex"
 	"flag"
+	"fmt"
+	"hash/crc64"
+	"io"
 	"os"
 	"os/signal"
-	"strings"
 	"testing"
-
-	logt "github.com/mendersoftware/go-lib-micro/log/testing"
 )
 
-var runAcceptanceTests bool
+const (
+	coverName = "coverage-acceptance"
+	coverExt  = ".txt"
+)
 
-// used for parsing '-cli-args' for urfave/cli when running acceptance tests
-// this is because of a conflict between urfave/cli and regular go flags required for testing (can't mix the two)
-var cliArgsRaw string
-
-func init() {
-	logt.MaybeDiscardLogs()
-
-	flag.BoolVar(&runAcceptanceTests, "acceptance-tests", false, "set flag when running acceptance tests")
-	flag.StringVar(&cliArgsRaw, "cli-args", "", "for passing urfave/cli args (single string) when golang flags are specified (avoids conflict)")
+func TestMain(m *testing.M) {
+	argHash := crc64.New(crc64.MakeTable(crc64.ECMA))
+	for _, arg := range os.Args {
+		_, _ = argHash.Write([]byte(arg))
+	}
+	fileNameCoverRun := fmt.Sprintf("%s@%s%s",
+		coverName, hex.EncodeToString(argHash.Sum(nil)), coverExt)
+	fileNameCoverAll := coverName + coverExt
+	os.Args = os.Args[:1]
+	flag.Set("test.run", "TestRunMain")
+	flag.Set("test.coverprofile", fileNameCoverRun)
+	exitCode := m.Run()
+	if exitCode > 0 {
+		os.Exit(exitCode)
+	}
+	// TODO: Should protect 'coverage-acceptance.txt' with a filelock
+	err := aggregateCoverage(fileNameCoverRun, fileNameCoverAll)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
 
 func TestRunMain(t *testing.T) {
-	flag.Parse()
-	if !runAcceptanceTests {
-		t.Skip()
-	}
-
-	// parse '-cli-args', remember about binary name at idx 0
-	var cliArgs []string
-
-	if cliArgsRaw != "" {
-		cliArgs = []string{os.Args[0]}
-		splitArgs := strings.Split(cliArgsRaw, " ")
-		cliArgs = append(cliArgs, splitArgs...)
-	}
-
-	go doMain(cliArgs)
-
 	stopChan := make(chan os.Signal)
 	signal.Notify(stopChan, os.Interrupt)
-
+	go func() {
+		doMain(os.Args[:cap(os.Args)])
+		signal.Stop(stopChan)
+		close(stopChan)
+	}()
 	<-stopChan
+}
+
+func aggregateCoverage(src, dst string) (err error) {
+	fdRun, err := os.OpenFile(src, os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open file '%s': %s\n", src, err)
+		os.Exit(1)
+	}
+	defer fdRun.Close()
+
+	var rSrc io.Reader = fdRun
+
+	fd, err := os.OpenFile(dst, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+	if os.IsExist(err) {
+		fd, err = os.OpenFile(dst, os.O_RDWR, 0644)
+		if err != nil {
+			return err
+		}
+		defer fd.Close()
+		_, err := fd.Seek(0, io.SeekEnd)
+		if err != nil {
+			return err
+		}
+		rdr := bufio.NewReader(fdRun)
+		// Skip first line
+		_, err = rdr.ReadSlice('\n')
+		if err != nil {
+			return err
+		}
+		rSrc = rdr
+	} else if err != nil {
+		return err
+	} else {
+		// Coverage file created
+		defer fd.Close()
+	}
+	_, err = io.Copy(fd, rSrc)
+	return err
 }
